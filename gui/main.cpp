63bee697f5b5b68204134e2cb1729c5d3f5d9f62
microcai <microcaicai@gmail.com>
2014-11-25 01:46:58 +0800

    cmake 用了啥招数啊, 需要用 winmain 这种入口点了, 囧

--- gui/main.cpp
+++ gui/main.cpp
@@ -2,6 +2,7 @@
 
 #include "app.hpp"
 
+#ifndef _WIN32
 int main(int argc, char *argv[])
 {
 	// 初始化该初始化的东西
@@ -11,3 +12,17 @@ int main(int argc, char *argv[])
 	// 开跑
 	return app.exec();
 }
+#else
+
+#include <windows.h>
+
+int _tWinMain(HINSTANCE hInstance, HINSTANCE hPrevInstance, LPSTR lpCmdLine, int nCmdShow)
+{
+	// 初始化该初始化的东西
+    OpenSSL_add_all_algorithms();
+	// 创建 QApp 对象
+    avimApp app(__argc, _targv);
+	// 开跑
+	return app.exec();
+}
+#endif
