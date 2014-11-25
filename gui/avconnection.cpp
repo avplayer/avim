6562297dcc605e8282e6c5b386ebccc1145d0827
microcai <microcaicai@gmail.com>
2014-11-25 12:46:40 +0800

    加点成员

--- gui/avconnection.cpp
+++ gui/avconnection.cpp
@@ -3,5 +3,11 @@
 
 AVConnection::AVConnection()
 {
+    qRegisterMetaType<std::shared_ptr<RSA>>("std::shared_ptr<RSA>");
+    qRegisterMetaType<std::shared_ptr<X509>>("std::shared_ptr<X509>");
 }
 
+void AVConnection::set_cert_and_key(std::shared_ptr<RSA> key, std::shared_ptr<X509> cert)
+{
+
+}
--- gui/avconnection.hpp
+++ gui/avconnection.hpp
@@ -3,17 +3,34 @@
 #include <memory>
 #include <boost/noncopyable.hpp>
 
+#include <openssl/rsa.h>
+#include <openssl/x509.h>
+
 #include <QObject>
 
 class avjackif;
 class AVConnection :  public QObject,  boost::noncopyable
 {
-    Q_OBJECT
+	Q_OBJECT
+public:
+	enum ConState{
+		DISCONNECTED,
+		CONNECTED,
+		AUTHORIZED,
+
+	};
 
 public:
     AVConnection();
+Q_SIGNALS:
+	void connection_state_change(ConState);
+
+public Q_SLOTS:
+	void set_cert_and_key(std::shared_ptr<RSA> key, std::shared_ptr<X509> cert);
 
 private:
 
+	std::shared_ptr<RSA> m_key;
+	std::shared_ptr<X509> m_cert;
 	std::shared_ptr<avjackif> m_avif;
 };
