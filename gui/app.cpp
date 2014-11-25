c6b25bc1d168fc728257a96a933ae6feef600282
microcai <microcaicai@gmail.com>
2014-11-26 00:21:21 +0800

    漏网之鱼

--- gui/app.cpp
+++ gui/app.cpp
@@ -11,8 +11,8 @@
 
 avimApp::avimApp(int argc, char* argv[])
 	: QApplication(argc, argv)
-	, m_io_work (m_io_service)
-	, m_avkernel (m_io_service)
+	, m_io_work(m_io_service)
+	, m_avkernel(m_io_service)
 {
 	// 开启 boost 线程跑 io_service
 	m_io_thread = std::thread([this]()
@@ -46,7 +46,7 @@ avimApp::~avimApp()
 	m_io_thread.join();
 }
 
-bool avimApp::load_key_and_cert (std::string cur_key, std::string cur_cert)
+bool avimApp::load_key_and_cert(std::string cur_key, std::string cur_cert)
 {
 	if (cur_key.empty())
 	{
@@ -64,7 +64,7 @@ bool avimApp::load_key_and_cert (std::string cur_key, std::string cur_cert)
 		cur_key = avim_cert.string();
 	}
 
-	if (!fs::exists(fs::path(cur_key)) || !fs::exists(fs::path (cur_cert)))
+	if (!fs::exists(fs::path(cur_key)) || !fs::exists(fs::path(cur_cert)))
 	{
 		std::cout << cur_key << '\n';
 		std::cout << cur_cert << '\n';
@@ -77,17 +77,21 @@ bool avimApp::load_key_and_cert (std::string cur_key, std::string cur_cert)
 	std::shared_ptr<BIO> bio_key {BIO_new_file(cur_key.c_str(), "r") , BIO_free};
 	std::shared_ptr<BIO> bio_cert {BIO_new_file(cur_cert.c_str(), "r") , BIO_free};
 
-	auto _c_rsa_key = PEM_read_bio_RSAPrivateKey(bio_key.get(), nullptr, [] (char * buf, int size, int rwflag, void * parent)->int
+	auto _c_rsa_key = PEM_read_bio_RSAPrivateKey(bio_key.get(), nullptr, [](char * buf, int size, int rwflag, void * parent)->int
 	{
 		// 打开窗口, 提升用户输入密码
 		bool ok;
-		QString text = QInputDialog::getText (nullptr, tr ("unlock key"),
-		tr ("the private key is password protected, please input the password:"), QLineEdit::Password,
-		"", &ok);
+		QString text = QInputDialog::getText(
+			nullptr,
+			tr("unlock key"),
+			tr("the private key is password protected, please input the password:"), QLineEdit::Password,
+			"",
+			&ok
+		);
 
 		if (ok && !text.isEmpty())
 		{
-			strncpy (buf, text.toUtf8().constData(), size);
+			strncpy(buf, text.toUtf8().constData(), size);
 			return text.toUtf8().length();
 		}
 		return -1;
@@ -101,20 +105,20 @@ bool avimApp::load_key_and_cert (std::string cur_key, std::string cur_cert)
 		// 现在暂时暴力退出
 
 		QMessageBox box;
-		box.setText ("unable to read the private key!");
+		box.setText("unable to read the private key!");
 		box.exec();
-		std::exit (1);
+		std::exit(1);
 	}
 
 	std::shared_ptr<X509> user_cert
 	{
-		PEM_read_bio_X509 (bio_cert.get(), NULL, NULL, NULL),
+		PEM_read_bio_X509(bio_cert.get(), NULL, NULL, NULL),
 		X509_free
 	};
 
 	// 设置下 cert 和密码
-	m_avconnection.reset (new AVConnection(m_io_service));
-	m_avconnection->set_cert_and_key (user_key, user_cert);
+	m_avconnection.reset(new AVConnection(m_io_service));
+	m_avconnection->set_cert_and_key(user_key, user_cert);
 	return true;
 }
 
@@ -126,16 +130,16 @@ int avimApp::exec()
 
 	if (auto_login == "true")
 	{
-		if (load_key_and_cert (m_cfg->get<std::string>("global.key"), m_cfg->get<std::string>("global.cert")))
+		if (load_key_and_cert(m_cfg->get<std::string>("global.key"), m_cfg->get<std::string>("global.cert")))
 		{
 			return start_main();
 		}
 		else
 		{
-			m_login_dialog.reset (new login_dialog(m_cfg.get()));
+			m_login_dialog.reset(new login_dialog(m_cfg.get()));
 			if (m_login_dialog->exec() == QDialog::Accepted)
 			{
-				if (!load_key_and_cert (m_login_dialog->get_key_path(), m_login_dialog->get_cert_path()))
+				if (!load_key_and_cert(m_login_dialog->get_key_path(), m_login_dialog->get_cert_path()))
 				{
 					return 1;
 				}
@@ -153,7 +157,7 @@ int avimApp::exec()
 		m_login_dialog.reset(new login_dialog(m_cfg.get()));
 		if (m_login_dialog->exec() == QDialog::Accepted)
 		{
-			if (!load_key_and_cert (m_login_dialog->get_key_path(), m_login_dialog->get_cert_path()))
+			if (!load_key_and_cert(m_login_dialog->get_key_path(), m_login_dialog->get_cert_path()))
 			{
 				return 1;
 			}
@@ -171,10 +175,10 @@ void avimApp::load_cfg()
 {
 	fs::path appdatadir = get_app_dir();
 
-	if (!fs::exists (appdatadir))
-		fs::create_directories (appdatadir);
+	if (!fs::exists(appdatadir))
+		fs::create_directories(appdatadir);
 
-	m_cfg.reset (new avim::ini (appdatadir / "config.ini"));
+	m_cfg.reset(new avim::ini(appdatadir / "config.ini"));
 }
 
 int avimApp::start_main()
@@ -188,7 +192,7 @@ int avimApp::start_main()
 	connect(m_avconnection.get(), SIGNAL(server_connected()), m_avconnection.get(), SLOT(start_login()), Qt::QueuedConnection);
 	// 要登录成功的消息!
 	connect(m_avconnection.get(), SIGNAL(login_success()), this, SIGNAL(login_success()), Qt::QueuedConnection);
-	connect(m_avconnection.get(), &AVConnection::login_success, std::bind(&AVConnection::handover_to_avkernel, m_avconnection.get(), std::ref (m_avkernel)));
+	connect(m_avconnection.get(), &AVConnection::login_success, std::bind(&AVConnection::handover_to_avkernel, m_avconnection.get(), std::ref(m_avkernel)));
 
 	m_mainwindow->show();
 	m_avconnection->start_connect();
@@ -221,14 +225,14 @@ MainWindow::MainWindow(avimApp* _avimapp)
 
 	connect(m_list, &QListWidget::itemDoubleClicked, [this] (QListWidgetItem * item)
 	{
-		Q_EMIT chat_opened (item->text().toStdString());
+		Q_EMIT chat_opened(item->text().toStdString());
 	});
 }
 
 void MainWindow::on_login_success()
 {
 	// TODO 更新 GUI 向用户显示
-	m_list->setEnabled (true);
+	m_list->setEnabled(true);
 }
 
 void MainWindow::on_lost_connection(int reason)
