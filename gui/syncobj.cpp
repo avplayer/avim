9861ccfcca3dc3d80850cf899083b272f7b74146
microcai <microcaicai@gmail.com>
2014-11-26 20:22:19 +0800

    style ...

--- gui/syncobj.cpp
+++ gui/syncobj.cpp
@@ -14,9 +14,9 @@ void SyncObjec::do_post(std::function<void()> func)
 {
     Q_EMIT post(func);
 }
+
 SyncObjec::SyncObjec()
 {
 	qRegisterMetaType<std::function<void()>>("std::function<void()>");
-	connect(this, &SyncObjec::post,
-			this, &SyncObjec::on_post, Qt::QueuedConnection);
+	connect(this, &SyncObjec::post, this, &SyncObjec::on_post, Qt::QueuedConnection);
 }
