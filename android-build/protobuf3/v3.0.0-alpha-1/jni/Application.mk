42464f5f1b02c409290ae81a64341032fdde3cc7
microcai <microcaicai@gmail.com>
2014-12-12 13:11:26 +0800

    Revert "protobuf3编译说明："
    This reverts commit d5dcd6642c31ae6edd1bde2424104c3f267fd5a3.
    
    readme.md 乱码, 修正后重新提交, 这个我先 revert 了.

--- android-build/protobuf3/v3.0.0-alpha-1/jni/Application.mk
+++
@@ -1,7 +0,0 @@
-APP_OPTIM := release  
-APP_STL               := gnustl_static
-APP_ABI               := all
-#APP_ABI              := all armeabi-v7a mips x86 mips64 x86_64
-APP_PROJECT_PATH 	  := $(call my-dir)
-APP_BUILD_SCRIPT      := $(call my-dir)/Android.mk
-APP_MODULES           := libprotobuf-cpp-lite libprotobuf-cpp-full
