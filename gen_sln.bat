841de3ede3ee3af9db8788df9c96287f59c5553b
microcai <microcaicai@gmail.com>
2014-12-15 00:00:12 +0800

    boost is using autolinking , not need to link to boost

--- gen_sln.bat
+++ gen_sln.bat
@@ -9,16 +9,15 @@ echo make sure your openssl installed on c:\openssl
 echo make sure protobuf is built first !!!!
 
 :: 每人的硬盘及使用情况都不一样，请勿统一目录,
-:: 各位根据自家情况把boost根目录设为BOOST_ROOT环境变量，OpenSSL同理
+:: 各位根据自家情况把boost根目录设为BOOST_ROOT环境变量
 set BOOST_ROOT=%BOOST_ROOT%
-set OPENSSL_ROOT=%OPENSSL_ROOT_DIR%
 set PROTOBUF_SRC_ROOT_FOLDER=%~dp0\third_party\protobuf-2.6.1\
 set CMAKE_PREFIX_PATH=c:\Qt\lib\cmake
 
 echo "making build directory"
-md build
+md %~dp0\build
 
-cd build
+cd %~dp0\build
 
 ECHO 1.Win64 build
 ECHO 2.Win32 build
@@ -27,7 +26,6 @@ ECHO.
 
 CHOICE /C 123 /M "Enter your choice:"
 
-:: Note - list ERRORLEVELS in decreasing order
 IF ERRORLEVEL 3 GOTO ENDSCRIPT
 IF ERRORLEVEL 2 GOTO Win32
 IF ERRORLEVEL 1 GOTO Win64
