16748fa1176948a8a589f5b48ca4aece33a2616b
microcai <microcaicai@gmail.com>
2014-12-22 19:02:46 +0800

    update fix

--- gen_sln.bat
+++ gen_sln.bat
@@ -11,7 +11,7 @@ echo make sure protobuf is built first !!!!
 :: 每人的硬盘及使用情况都不一样，请勿统一目录,
 :: 各位根据自家情况把boost根目录设为BOOST_ROOT环境变量
 set BOOST_ROOT=%BOOST_ROOT%
-set PROTOBUF_SRC_ROOT_FOLDER=%~dp0\third_party\protobuf-2.6.1\
+set PROTOBUF_SRC_ROOT_FOLDER=C:\protobuf-2.6.1\
 set CMAKE_PREFIX_PATH=c:\Qt\lib\cmake
 
 echo "making build directory"
